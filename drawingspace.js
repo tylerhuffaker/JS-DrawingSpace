82b6d8855721a15fa9b66323c862f6ff89fb2cf4
Tyler Huffaker <43476102+tylerhuffaker@users.noreply.github.com>
2019-03-01 10:54:12 -0500

    Version 3.0 Update

--- drawingspace.js
+++ drawingspace.js
@@ -1,11 +1,12 @@
 // im very proud of myself for being able to make this and you should be too.
 // essentially this creates a cool little drawing board thing for an artist or somthing.
+var rdmcol = false;
 function start(){
     keyDownMethod(keyDown);
     println("Press 'S' to change shape");
     println("Press 'C' to clear ART board");
+    println("Press 'R' to enable RNDM color mode");
     var shtype = readLine("Choose Shape type: (square or ball): ");
-    var rdmcol = readBoolean("Enable Random Color Mode? (True/False): ");
     if (shtype == "ball"){
     mouseClickMethod(addBall);
     mouseDragMethod(addBall);
@@ -35,6 +36,17 @@ function start(){
 	            removeAll();
 	        }else if(e.keyCode == Keyboard.letter('s')){
 	            shapeChange();
+	        }else if(e.keyCode == Keyboard.letter('r')){
+	            if(rdmcol == false){
+	            ranm();
+	            }else{
+	                println("Random Colors: Inactive");
+	                rdmcol = false;
+	            }
+	        }
+	        function ranm(){
+	            println("Random Colors: Active");
+	            rdmcol = true;
 	        }
 	   function shapeChange(e){
 	       if(shtype == "ball"){
